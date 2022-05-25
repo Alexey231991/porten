c8547c9295d4412b9694f34661d46e066d702cf6
Alexey231991 <79451868+Alexey231991@users.noreply.github.com>
2022-05-25 16:25:56 +0300

    add files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.header__burger').click(function(event){
+    $('.header__burger, .header_menu_list').toggleClass('active');
+    $('body').toggleClass('lock');
+    });
+    $('.header_menu_list a').click(function(event){
+      $('.header__burger, .header_menu_list').toggleClass('active');
+      $('body').toggleClass('lock');
+    });
+  });
+  
